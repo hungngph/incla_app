cb57ffade3a86f6a2820343fe681b7cb6d324819
Nguyễn Phi Hùng <17520068@gm.uit.edu.vn>
2019-11-02 17:46:17 +0700

    update MainPage

---
+++ lib/screens/UserPage/UserPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class UserPage extends StatelessWidget {
+  
+  const UserPage();
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+      body: Center(
+        child: Text(
+          'User Page',
+        ),
+      ),
+    );
+  }
+}
